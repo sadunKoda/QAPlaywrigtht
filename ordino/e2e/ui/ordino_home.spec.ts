df3e0f89d1d5d2a05c9988b8379ec5c759cdb03e
sandun <msadunkodagoda@gmail.com>
2025-12-18 13:12:36 +0530

    new 2

--- ordino/e2e/ui/ordino_home.spec.ts
+++ ordino/e2e/ui/ordino_home.spec.ts
@@ -8,5 +8,57 @@ test.describe('Ordino Home Dashboard - Test Suite', () => {
         await loginPage.step_enterPassword("admin");
         await loginPage.step_clickLogin();           
         await homePage.step_logout();
-    });    
+    });  
+
+    
+    test('Verify Profile Logout 2', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+
+     
+    test('Verify Profile Logout 3', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+    
+    test('Verify Profile Logout 4 ', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+    
+    test('Verify Profile Logout 5', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+    
+    test('Verify Profile Logout 6', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+    
+    test('Verify Profile Logout 7', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@platform.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });  
+    
+    
 });
