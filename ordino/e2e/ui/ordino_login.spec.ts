253892ed153005373326356b297da84f0c2df62e
sandun <msadunkodagoda@gmail.com>
2025-12-18 13:14:38 +0530

    new 3

--- ordino/e2e/ui/ordino_login.spec.ts
+++ ordino/e2e/ui/ordino_login.spec.ts
@@ -17,4 +17,44 @@ test.describe('Ordino Auth Sign-in - Test Suite', () => {
     await loginPage.step_clickLogin()
   })
 
+  test('Auth Sign-in with In-Valid Credentials 2', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
+  test('Auth Sign-in with In-Valid Credentials 3 ', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
+  test('Auth Sign-in with In-Valid Credentials 4', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
+  test('Auth Sign-in with In-Valid Credentials 5', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
+  test('Auth Sign-in with In-Valid Credentials 6', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
 });
